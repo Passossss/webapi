59ac6dbe1251d0537db2dea36de6ba36ed1cee0f
gu-ubuntu <you@example.com>
2025-02-22 01:23:17 -0300

    addcode

--- Models/PersonModel.cs
+++ Models/PersonModel.cs
@@ -1,12 +1,19 @@
 class PersonModel
 {
-    public int Id { get; private set; }
-    public string Name { get; set; }
+  public int Id { get; private set; }
+  public string Name { get; set; }
 
-    /*public static PersonModel(string name)
-    {
-      removido conteudo
-      reafazer!
+  /*public static PersonModel(string name)
+  {
+    removido conteudo
+    reafazer!
 
-    }*/
+  }*/
+  public static PersonModel Create(int id, string name)
+  {
+
+
+    int idade;
+    return new PersonModel;
+  }
 }
